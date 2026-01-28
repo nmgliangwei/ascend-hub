f8cbcaa2d0460e04d607e8b949762c72e45fb92c
梁伟 <362253310@qq.com>
2026-01-28 19:25:26 +0800

    Update start.sh

--- mis-tei/start.sh
+++ mis-tei/start.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 # Copyright © Huawei Technologies Co., Ltd. 2024. All rights reserved.
 
-if [[ "$#" -ne 4 ]]; then
-    echo "Need param: <model_id> <listen_ip> <listen_port> <served_model_name>"
+if [[ "$#" -ne 3 ]]; then
+    echo "Need param: <model_id> <listen_ip> <listen_port>"
     exit 1
 fi
 
+# remove the last '/'
+#MODEL_ID=$(echo "$1" | cut -d "=" -f2 | sed "s/\/*$//g")
 MODEL_ID=$1
 LISTEN_IP=$2
 LISTEN_PORT=$3
-#MODEL_DIR=$4
-SERVED_MODEL_NAME=$4
-MODEL_NAME=$(echo ${MODEL_ID} | cut -d'/' -f2)
-MODEL_MEMORY_LIMIT=2048
-SUPPORT_MODELS=("BAAI/bge-large-zh-v1.5")
+
+MODEL_DIR="/home/HwHiAiUser/model"
 
 sleep 3
 
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 source /usr/local/Ascend/nnal/atb/set_env.sh
-export LD_PRELOAD=$(ls /usr/local/lib/python3.11/dist-packages/scikit_learn.libs/libgomp-*):$LD_PRELOAD
+export LD_PRELOAD=$(ls /usr/local/lib/python3.11/site-packages/scikit_learn.libs/libgomp-*):$LD_PRELOAD
 export PATH="/home/HwHiAiUser/.cargo/bin:$PATH"
 
 if [[ -n $(id | grep uid=0) ]];then
@@ -27,16 +26,6 @@ if [[ -n $(id | grep uid=0) ]];then
 else
     source /home/HwHiAiUser/Ascend/mxRag/script/set_env.sh
 fi
-
-function check_model_support() {
-    if [[ "${SUPPORT_MODELS[*]}" =~ ${MODEL_NAME} ]]; then
-        echo "Support model $MODEL_NAME."
-        return 0
-    else
-        echo "$MODEL_NAME dose not support."
-        return 1
-    fi
-}
 
 function check_model_exists() {
     if [[ ! -e "${MODEL_ID}/config.json" ]]; then
@@ -68,60 +57,18 @@ function download_model() {
 
 function start_tei_service() {
     if [[ -z ${TEI_NPU_DEVICE} ]]; then
-        if ! select_device; then
-            echo "Available device not found"
-            exit 1
-        fi
+        export TEI_NPU_DEVICE=0
+        echo "run on device 0"
     fi
     echo "Starting TEI service on ${LISTEN_IP}:${LISTEN_PORT}..."
+    #cd "${MODEL_DIR}"
     text-embeddings-router \
       --model-id "${MODEL_ID}" \
       --port "${LISTEN_PORT}" \
-      --hostname "${LISTEN_IP}" \
-      --served-model-name "{$SERVED_MODEL_NAME}"
-}
-
-function select_device() {
-    echo "test npu-smi info"
-    npu-smi info
-    ret=$?
-    if [[ $ret -ne 0 ]]; then
-        echo "test npu-smi info failed"
-        return 1
-    fi
-
-
-    while IFS=' ' read -r npu_id chip_id chip_logic_id; do
-        if [[ $chip_logic_id =~ ^[0-9]+$ ]]; then
-            local memory_type="DDR"
-            chip_type=$(npu-smi info -t board -i "$npu_id" -c "$chip_id" | awk -F ":" '/Chip Name/ {print $2}' | sed 's/^[ \t]*//')
-            if [[ $chip_type =~ ^910 ]]; then
-                memory_type="HBM"
-            fi
-            local total_capacity
-            local usage_rate
-            local avail_capacity
-            total_capacity=$(npu-smi info -t usages -i "$npu_id" -c "$chip_id" | grep "$memory_type Capacity(MB)" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
-            usage_rate=$(npu-smi info -t usages -i "$npu_id" -c "$chip_id" | grep "$memory_type Usage Rate(%)" | cut -d ":" -f 2 | sed 's/^[ \t]*//')
-            avail_capacity=$(awk "BEGIN {printf \"%d\", $total_capacity - $total_capacity * ($usage_rate / 100)}")
-            echo "NPU_ID: $npu_id, CHIP_ID: $chip_id， CHIP_LOGIC_ID: $chip_logic_id CHIP_TYPE: $chip_type, MEMORY_TYPE: $memory_type, CAPACITY: $total_capacity, USAGE_RATE: $usage_rate, AVAIL_CAPACITY: $avail_capacity"
-            if [[ $avail_capacity -gt $MODEL_MEMORY_LIMIT ]]; then
-                export TEI_NPU_DEVICE="$chip_logic_id"
-                echo "Using NPU: $chip_logic_id start TEI service"
-                return 0
-            fi
-        fi
-    done <<< "$(npu-smi info -m | awk 'NR>1 {print $1, $2, $3}')"
-    return 1
+      --hostname "${LISTEN_IP}"
 }
 
 function main() {
-:<<!
-    if ! check_model_support; then
-        echo "Check '<model_id>' failed"
-        exit 1
-    fi
-!
     if ! check_model_exists; then
         #if ! download_model; then
         #    echo "Download model ${MODEL_ID} failed"
